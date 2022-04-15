0ee24304c6f3d5213e14e5111d6e0d9c5d9cfe50
HXLLL <1150189655@qq.com>
2022-04-15 22:34:02 +0800

    external interface of controller

--- controller.py
+++ controller.py
@@ -9,6 +9,7 @@ def update(dir: str) -> None:
     repo = storage.load_repo()
     stage = storage.load_stage()
 
+    # get all file's hash values from stage
     stage.add_update(dir)
     for m in stage.modify_sequence:
         m.apply()
@@ -16,4 +17,56 @@ def update(dir: str) -> None:
     storage.save_repo(repo)
     storage.save_stage(stage)
 
+
+def update_add(src: str, dst: str) -> None:
+    repo = storage.load_repo()
+    stage = storage.load_stage()
+
+    # TODO 
+
+    storage.save_repo(repo)
+    storage.save_stage(stage)
+
+
+def transform(dir1: str, entry: str, msg: str, is_map: bool, dir2: str = ".") -> None:
+    repo = storage.load_repo()
+    stage = storage.load_stage()
+
+    storage.save_repo(repo)
+    storage.save_stage(stage)
+
+
+def commit(msg: str) -> None:
+    pass
+
+
+def checkout(obj: str) -> None:
+    pass
+
+
+def save(obj: str) -> None:
+    # load metadata
+    # save metadata
+    pass
+
+
+def unsave(obj: str) -> None:
+    # load metadata
+    # save metadata
+    pass
+
+
+def adjust() -> None:
+    pass
+
+
+def log() -> str:
+    pass
+
+
+def status() -> str:
+    pass
+
+
+def branch(name: str) -> str:
     pass
