248f63bf72ef71505fca98d40d6cca2ff1d6ec54
xxccxcxc <784066071@qq.com>
2022-04-16 20:27:49 +0800

    增加stage.py,directory.py,blob.py,修改Version

--- version.py
+++ version.py
@@ -1,45 +1,23 @@
 from typing import List
 from modify import Modify
-from transfrom import Transform
+from transform import Transform
 from update import Update
-import storage
-from file_info import File_info
+from storage import storage
+from blob import File_info
+from stage import Stage
 
 
 class Version():
-    def __init__(self):
-        self.modify_sequence = []
+    def __init__(self, stage: Stage, message: str, parent: 'Version') -> None:
+        '''
+        功能:复制一个stage,并添加commit信息,构造完的Version会被直接挂到提交树上
+        '''
+        self.__modify_sequence = stage.get_modify_sequence()
+        self.__message = message
+        self.__parent = stage.get_parent()
 
-    
-
-class Stage(Version):
-    def __init__(self):
-        super(Stage, self).__init__()
-        self.modify_sequence = []
-
-    # 仅stage调用
-    def scan_different_files() -> list, list : 
-        pass
-
-    def add_update(self, dir: str) -> None:
-        m = Update()
-        add_list_bypath, del_list_bypath = scan_different_files()
-        for add_file in m.add_list:
-            path_in_datagit = storage.save_file(add_file)
-            
-            m.add_list.append( File_info(path_in_datagit, add_file) ) # type: list[file_info]
-            
-        self.modify_sequence.append(m)
-        for File_name in m.file_sequence:
-            storage.savefile(File_name)
-    
-    
-    def transform(self, dir1, entry, isMap, dir2):
-        dir_in_datagit = storage.save_transform(dir1, entry, isMap, dir2) # type: string
-        m = Transform(dir_in_datagit, dir1, entry, isMap, dir2)
-        self.modify_sequence.append(m)
-
-        m.apply()
-
-    def status(self):
+    def calc_hash() -> str:
+        '''
+        功能:计算Version的哈希值
+        '''
         pass
