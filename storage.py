7c89afea3ce97dfbb1ea5eae9c98a3d27af7c6f0
HXLLL <1150189655@qq.com>
2022-04-16 18:58:57 +0800

    change storage to a class

--- storage.py
+++ storage.py
@@ -2,21 +2,30 @@ from file_info import File_info
 from repo import Repo
 from version import Version
 
+class Storage:
+    def __init__():
+        pass
 
-def load_repo() -> Repo:
-    return Repo()
+    def load_storage() -> None:
+        pass
+
+    def save_storage() -> None:
+        pass
+
+    def load_repo() -> Repo:
+        return Repo()
+
+    def load_stage() -> Version:
+        return Version()
+
+    def save_repo(repo: Repo) -> None:
+        pass
+
+    def save_stage(stage: Version) -> None:
+        pass
+
+    def save_file(file_name: str) -> File_info:
+        pass
 
 
-def load_stage() -> Version:
-    return Version()
-
-
-def save_repo(repo: Repo) -> None:
-    pass
-
-
-def save_stage(stage: Version) -> None:
-    pass
-
-def save_file(file_name: str) -> File_info:
-    pass
+storage = Storage()
