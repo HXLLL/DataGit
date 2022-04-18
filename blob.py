28ae477186b42d96c9adadc69982f50a8a7f5e9b
xxccxcxc <784066071@qq.com>
2022-04-18 19:33:17 +0800

    添加Dir.unfold(),Blob.unfold()

--- blob.py
+++ blob.py
@@ -1,4 +1,12 @@
+from sympy import root
+from blob import Blob
+from typing import List, Tuple
+import os
+
 class Blob:
     def __init__(self) -> None:
         self.name = 'null'  # 文件名称
         self.hash = 'null'  # 文件哈希值
+
+    def unfold(self, root_path: str) -> List[Tuple[str, Blob]]:
+        return [(os.path.join(root_path, self.name), self)]
