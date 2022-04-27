09f3a45d2a0eba5c1f317eb96c78ba975440062a
HUANG Xiaolong <huangxiaolong@pku.edu.cn>
2022-04-27 20:48:03 +0800

    scale transformer

---
+++ transformer/scale/scale.py
@@ -0,0 +1,19 @@
+import cv2
+import sys
+import os
+from tqdm import tqdm
+
+scale = 0.6
+
+for f in tqdm(os.listdir(sys.argv[1])):
+    if f == ".datagit":
+        continue
+    f_dir = os.path.join(sys.argv[1], f)
+    img = cv2.imread(f_dir)
+
+    w = int(img.shape[1] * scale)
+    h = int(img.shape[0] * scale)
+
+    new = cv2.resize(img, (w, h))
+
+    cv2.imwrite(f_dir, new)
